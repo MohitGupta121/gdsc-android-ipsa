82a5769c65898881146a59884ab4c14950565e07
Moiz Bohra <90457675+Moizcode@users.noreply.github.com>
2022-10-07 00:07:22 +0530

    CertificateVerificationActivityUI

--- app/src/main/java/com/mohit/gdsc/ipsacademy/ui/screens/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/mohit/gdsc/ipsacademy/ui/screens/homeScreen/HomeScreen.kt
@@ -1,18 +1,22 @@
 package com.mohit.gdsc.ipsacademy.ui.screens.homeScreen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import android.content.Intent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mohit.gdsc.ipsacademy.ui.screens.CertificateVerificationActivity
 
 @Composable
 fun HomeFragmentUI() {
@@ -36,6 +40,32 @@ fun HomeFragmentUI() {
             modifier = Modifier.padding(horizontal = 10.dp)
         )
         PastEventsDetailsDetailsContent()
+        Spacer(modifier = Modifier.height(100.dp))
+        Text(
+            text = "Verify Certificate",
+            style = TextStyle(
+                color =  Color.Black,
+                fontSize = 22.sp
+            ),
+            modifier = Modifier.padding(10.dp)
+        )
+        Text(
+            text = "Verify the authenticity of the Certificates Issued by Google Developer Student Club IES-IPSA. Enter the verification code given on the certificates to check it's authenticity. Click Below :",
+            style = TextStyle(
+                fontSize = 15.sp
+            ),
+            modifier = Modifier.padding(horizontal = 10.dp),
+            textAlign = TextAlign.Justify
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = {
+                mContext.startActivity(Intent(mContext, CertificateVerificationActivity::class.java)) },
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.White), border = BorderStroke(1.dp,
+                    Color.Green) ) {
+                Text(text = "Certificate Verification", color = Color.Green)
+            }
+        }
         Spacer(modifier = Modifier.height(100.dp))
     }
 
